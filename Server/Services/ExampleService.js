6bae4eac98e4d6211d1e14e61cbe735a9193b34d
havenfricke <havenfricke@u.boisestate.edu>
2026-02-22 12:40:39 -0700

    class wrappers on all modules

--- Server/Services/ExampleService.js
+++ Server/Services/ExampleService.js
@@ -2,55 +2,55 @@ const exampleRepository = require('../Repositories/ExampleRepository');
 const Example = require('../Models/Example');
 const IdGen = require('../Utils/IdGen');
 
-async function getAllExamples(queryParams) {
-  const examples = await exampleRepository.getAllExamples(queryParams);
-  return examples.map(examples => new Example(examples.id, examples.name));
-}
+class ExampleService {
 
-async function getExampleById(id) {
-  const example = await exampleRepository.getExampleById(id);
-  if (!example) {
-    throw new Error('Example not found');
-  }
-  return new Example(example.id, example.name);
-}
-
-async function createExample(body) {
-  const created = await exampleRepository.createExample(id = IdGen.getId(), body);
-  return new Example(created.id, created.name);
-}
-
-async function editExample(update) {
-  const original = await exampleRepository.getExampleById(update.id);
-  if (!original) {
-    throw new Error("Example not found");
+  async getAllExamples(queryParams) {
+    const examples = await exampleRepository.getAllExamples(queryParams);
+    return examples.map(examples => new Example(examples.id, examples.name));
   }
 
-  const updatedExample = {
-    name: update.name === original.name ? original.name : update.name
-    // Add other example props here and use ternary to update
-  };
-
-  const updated = await exampleRepository.editExample(original.id, updatedExample);
-  return new Example(updated.id, updated.name);
-}
-
-async function deleteExample(id) {
-  const example = await exampleRepository.getExampleById(id);
-  if (!example) {
-    throw new Error("Example not found");
+  async getExampleById(id) {
+    const example = await exampleRepository.getExampleById(id);
+    if (!example) {
+      throw new Error('Example not found');
+    }
+    return new Example(example.id, example.name);
   }
-  await exampleRepository.deleteExample(id);
-  return { message: "Example deleted successfully" };
+
+  async createExample(body) {
+    const created = await exampleRepository.createExample(id = IdGen.getId(), body);
+    return new Example(created.id, created.name);
+  }
+
+  async editExample(update) {
+    const original = await exampleRepository.getExampleById(update.id);
+    if (!original) {
+      throw new Error("Example not found");
+    }
+
+    const updatedExample = {
+      name: update.name === original.name ? original.name : update.name
+      // Add other example props here and use ternary to update
+    };
+
+    const updated = await exampleRepository.editExample(original.id, updatedExample);
+    return new Example(updated.id, updated.name);
+  }
+
+  async deleteExample(id) {
+    const example = await exampleRepository.getExampleById(id);
+    if (!example) {
+      throw new Error("Example not found");
+    }
+    await exampleRepository.deleteExample(id);
+    return { message: "Example deleted successfully" };
+  }
+  
 }
 
-module.exports = {
-  getAllExamples,
-  getExampleById,
-  createExample,
-  editExample,
-  deleteExample
-};
+
+
+module.exports = new ExampleService();
 
 // the service layer acts as the heart of your 
 // application by housing the business logic, 
